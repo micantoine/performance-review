d20276520f1b788a6ded0dd299768903421143c8
Michael Vurchio <michael@micantoine.dev>
2020-10-10 22:37:44 +0900

    Fix error formatting

--- server/src/utils/formatErrorMessages.ts
+++ server/src/utils/formatErrorMessages.ts
@@ -1,20 +1,12 @@
 export interface MessageInterface {
   context: string;
   message: Array<string>;
-  [Symbol.iterator]: () => Generator;
 }
 
-function formatErrorMessages(message: Array<string>, context = 'details'): MessageInterface {
-  return {
+function formatErrorMessages(message: Array<string>, context = 'details'): [MessageInterface] {
+  return [{
     context,
-    message,
-    * [Symbol.iterator]() {
-      const properties = Object.keys(this);
-      // eslint-disable-next-line no-restricted-syntax
-      for (const i of properties) {
-        yield [i, this[i]];
-      }
-    }
-  };
+    message
+  }];
 }
 export default formatErrorMessages;
