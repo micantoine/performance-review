f94113ed4f6dd932004d97b09ed3a8c3aee2ec04
Michael Vurchio <michael@micantoine.dev>
2020-10-11 17:17:15 +0900

    Make sure admin is one for seed purpose

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -7,6 +7,9 @@ import departments from './deparments.json';
 db.sequelize.sync({ force: true })
   .then(async () => {
     await Promise.all(departments.map((department) => db.Department.create(department)));
+    // make sure admin has Id 1 for seed purpose
+    await db.User.create(users[0]);
+    delete users[0];
     await Promise.all(users.map((user) => db.User.create(user)));
     await Promise.all(reviews.map((review) => db.Review.create(review)));
   });
